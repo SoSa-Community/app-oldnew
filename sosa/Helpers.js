3be8cd0f143095ab6b00c62c1c4bde1d6fb87324
James <james.mahy@cevo.co.uk>
2020-04-10 19:35:06 +0100

    Implemented fetch and fetch with timeout

--- sosa/Helpers.js
+++ sosa/Helpers.js
@@ -1,6 +1,53 @@
 import {Alert} from 'react-native';
+import {SoSaConfig} from "./config";
 
 export default class Helpers {
+
+    static fetchWithTimeout = (url, options, timeout) => {
+        if(typeof(timeout) === "undefined") timeout = 3000;
+
+        let fetchPromise = fetch(url, options);
+        return new Promise((resolve, reject) => {
+            if(timeout !== null)    setTimeout(() => reject('Network Timed Out'), timeout);
+            fetchPromise.then(resolve, reject);
+        });
+    };
+
+    static request = (namespace, data, post=true) => {
+
+        let uri = `${SoSaConfig.auth.server}/${namespace}`;
+        let headers = {
+            method: post?'POST':'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            }
+        };
+        console.log('Headers', headers);
+
+        if(data){
+            if(post){
+                headers.body = JSON.stringify(data);
+            }else{
+                let keyValues = [];
+                console.log(data);
+                data.forEach((value, key) => keyValues.push(`${key}=${value}`));
+                uri += `?${keyValues.join('&')}`;
+            }
+        }
+
+        console.log('API Request', namespace, data, uri);
+
+        return Helpers.fetchWithTimeout(uri, headers).then((response) => {
+            console.log('Response', response);
+            try {
+                return response.json();
+            }catch (e) {
+                console.log(e);
+            }
+        });
+    };
+
     static generateRand = () => {
         return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     };
