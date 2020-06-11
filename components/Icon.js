e81332bb9539e6c326034fea912b3e3eb0c9e7c5
James <james.mahy@cevo.co.uk>
2020-06-11 18:30:29 +0100

    Fixed Icon styling

--- components/Icon.js
+++ components/Icon.js
@@ -4,7 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
 export default class Icon extends Component {
 
     render() {
-        return (<FontAwesomeIcon icon={this.props.icon}  style={this.props.style} size={this.props.size} />)
+        return (<FontAwesomeIcon icon={this.props.icon} style={this.props.style} size={this.props.size} color={this.props.color} onPress={this.props.onPress}/>)
     }
 
 }
