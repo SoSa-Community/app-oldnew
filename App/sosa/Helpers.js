5a84876ad7c51a6c1b240789dcc8477808345975
James <james.mahy@cevo.co.uk>
2020-11-28 20:02:19 +0000

    Backup for atob

--- App/sosa/Helpers.js
+++ App/sosa/Helpers.js
@@ -31,6 +31,45 @@ export default class Helpers {
 	};
 
 	static base64Decode(input) {
+	    if(typeof(atob) !== 'function'){
+            let keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+            let output = null;
+            let chr1, chr2, chr3 = "";
+            let enc1, enc2, enc3, enc4 = "";
+            let i = 0;
+        
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            let base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (!base64test.exec(input)) {
+                output = "";
+                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+            
+                do {
+                    enc1 = keyStr.indexOf(input.charAt(i++));
+                    enc2 = keyStr.indexOf(input.charAt(i++));
+                    enc3 = keyStr.indexOf(input.charAt(i++));
+                    enc4 = keyStr.indexOf(input.charAt(i++));
+                
+                    chr1 = (enc1 << 2) | (enc2 >> 4);
+                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                    chr3 = ((enc3 & 3) << 6) | enc4;
+                
+                    output = output + String.fromCharCode(chr1);
+                
+                    if (enc3 !== 64) {
+                        output = output + String.fromCharCode(chr2);
+                    }
+                    if (enc4 !== 64) {
+                        output = output + String.fromCharCode(chr3);
+                    }
+                
+                    chr1 = chr2 = chr3 = "";
+                    enc1 = enc2 = enc3 = enc4 = "";
+                
+                } while (i < input.length);
+            }
+            return output;
+        }
         return atob(input.replace(/[^A-Za-z0-9\+\/\=]/g, ""));
 	}
     
