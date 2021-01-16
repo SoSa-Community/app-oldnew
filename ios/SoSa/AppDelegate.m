8b1fa62cf4592bbaa11e7072723e5432c6b65376
James <james@Jamess-2018-Mac-Mini.local>
2021-01-16 12:12:09 +0000

    App center fixes for iOS

--- ios/SoSa/AppDelegate.m
+++ ios/SoSa/AppDelegate.m
@@ -4,6 +4,9 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -34,6 +37,10 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+  
     #ifdef FB_SONARKIT_ENABLED
         InitializeFlipper(application);
     #endif
