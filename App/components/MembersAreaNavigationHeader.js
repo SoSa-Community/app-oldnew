b205a3aefd8c921af6d4b4203bb145a035bca062
James <james.mahy@cevo.co.uk>
2021-01-18 10:16:15 +0000

    Converted MembersAreaNavigationHeader to functional component

--- App/components/MembersAreaNavigationHeader.js
+++ App/components/MembersAreaNavigationHeader.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
-import {Keyboard, Text, View, StyleSheet, BackHandler} from "react-native";
-
+import React, { useState, forwardRef, useImperativeHandle } from 'react';
+import { Keyboard, Text, View, StyleSheet, BackHandler } from "react-native";
 import { useFocusEffect } from '@react-navigation/native';
+import PropTypes from "prop-types";
 
 import IconButton from "./IconButton";
 import BaseStyles from "../screens/styles/base";
+
+
 
 const Styles = StyleSheet.create({
     menuTop: {
@@ -18,102 +20,113 @@ const Styles = StyleSheet.create({
     menuTopRight: {paddingRight: 10, flex:0, flexDirection: 'row', alignItems: 'center', justifyContent:'flex-end'}
 });
 
-function HandleBackButton({ onBack }) {
-    useFocusEffect(
-        React.useCallback(() => {
-            BackHandler.addEventListener('hardwareBackPress', onBack);
-            return () => BackHandler.removeEventListener('hardwareBackPress', onBack);
-        }, [])
-    );
-    return null;
-}
-
-export default class MembersAreaNavigationHeader extends Component {
-    state = {
-        headerIcons: []
-    };
-
-    constructor(props) {
-        super();
-        this.state.headerIcons = props.icons;
-    }
-
-    addHeaderIcon = (id, icon, onPress) => {
-        let icons = this.state.headerIcons;
-        let found = false;
-
-        icons.forEach((item, index) => {
-            if(item.id === id){
-                found = true;
-                item.icon = icon;
-                item.onPress = onPress;
-            }
-        });
-
-        if(!found){
-            icons.unshift({
-                id: id,
-                icon: icon,
-                onPress: onPress
+const MembersAreaNavigationHeader = forwardRef((
+    { title, onBack, icons, showLeftMenu, leftMenuMode, showRightMenu, membersNavigation, drawerNavigation },
+    ref
+) =>
+{
+    let leftMenu = null;
+    let rightMenu = null;
+    
+    const [headerIcons, setHeaderIcons] = useState(icons);
+    
+    useImperativeHandle(ref, () => ({
+        
+        addHeaderIcon(id, icon, onPress){
+            let icons = [...headerIcons];
+            let found = false;
+        
+            icons.forEach((item, index) => {
+                if(item.id === id){
+                    found = true;
+                    item.icon = icon;
+                    item.onPress = onPress;
+                }
             });
+        
+            if(!found){
+                icons.unshift({
+                    id: id,
+                    icon: icon,
+                    onPress: onPress
+                });
+            }
+        
+            setHeaderIcons(icons);
+        },
+        removeHeaderIcon(id) {
+            setHeaderIcons(headerIcons.filter(item => (item.id !== id)));
         }
-
-        this.setState({headerIcons: icons});
-    };
+    }))
     
-    removeHeaderIcon = (id) => {
-        let icons = this.state.headerIcons;
-        let found = false;
-    
-        icons.forEach((item, index) => {
-            if(item.id === id) delete icons[index];
-        });
-    
-        this.setState({headerIcons: icons});
-    }
-
-    render() {
-
-        let leftMenu = null;
-        let rightMenu = null;
-
-        if(this.props.showLeftMenu){
-            let menuIcon = null;
-            if(this.props.leftMenuMode === 'back'){
-                menuIcon = <IconButton icon={['fal', 'chevron-left']} style={{color: '#CCC'}} size={22} onPress={() => {
-                    if(typeof(this.props.onBack) === 'function') {
-                        try {
-                            this.props.onBack();
-                        } catch (e) {
-                            console.debug('Go Back Went Wrong', e);
-                        }
+    if(showLeftMenu){
+        let menuIcon = null;
+        if(leftMenuMode === 'back'){
+            if(onBack) {
+                useFocusEffect(
+                    React.useCallback(() => {
+                        BackHandler.addEventListener('hardwareBackPress', onBack);
+                        return () => BackHandler.removeEventListener('hardwareBackPress', onBack);
+                    }, [])
+                );
+            }
+            
+            menuIcon = <IconButton icon={['fal', 'chevron-left']} style={{color: '#CCC'}} size={22} onPress={() => {
+                if(typeof(onBack) === 'function') {
+                    try {
+                        onBack();
+                    } catch (e) {
+                        console.debug('Go Back Went Wrong', e);
                     }
-                    this.props.membersNavigation?.current?.goBack();
-                }}/>
-            }else{
-                menuIcon = <IconButton icon={['fal', 'bars']} style={{color: '#CCC'}} size={22} onPress={() => {
-                    Keyboard.dismiss();
-                    this.props.drawerNavigation.dangerouslyGetParent().openDrawer()
-                }}/>;
-            }
-            leftMenu = <View style={Styles.menuTopLeft}>{menuIcon}</View>;
+                }
+                membersNavigation?.current?.goBack();
+            }}/>
+        }else{
+            menuIcon = <IconButton icon={['fal', 'bars']} style={{color: '#CCC'}} size={22} onPress={() => {
+                Keyboard.dismiss();
+                drawerNavigation.dangerouslyGetParent().openDrawer()
+            }}/>;
         }
-
-        if(this.props.showRightMenu) {
-            let icons = this.state.headerIcons.map((item, i) => {
-                return (<View style={{verticalAlign:'center', marginLeft: 10}} key={item.id}>
-                    <IconButton icon={item.icon} style={{color: '#CCC'}} activeStyle={{color: '#444442'}} size={30} onPress={item.onPress}/>
-                </View>);
-            });
-            rightMenu = <View style={Styles.menuTopRight}>{icons}</View>;
-        }
-
-
-        return  <View style={Styles.menuTop}>
-                    { this.props.leftMenuMode === 'back' && <HandleBackButton onBack={this.props.onBack} /> }
-                    {leftMenu}
-                    <Text style={BaseStyles.headerTitle}>{this.props.title}</Text>
-                    {rightMenu}
-                </View>
+        leftMenu = <View style={Styles.menuTopLeft}>{menuIcon}</View>;
     }
-}
+
+    if(showRightMenu) {
+        let icons = headerIcons.map((item, i) => {
+            return (<View style={{verticalAlign:'center', marginLeft: 10}} key={item.id}>
+                <IconButton icon={item.icon} style={{color: '#CCC'}} activeStyle={{color: '#444442'}} size={30} onPress={item.onPress}/>
+            </View>);
+        });
+        rightMenu = <View style={Styles.menuTopRight}>{icons}</View>;
+    }
+
+    return  <View style={Styles.menuTop}>
+                {leftMenu}
+                <Text style={BaseStyles.headerTitle}>{title}</Text>
+                {rightMenu}
+            </View>
+ 
+});
+
+MembersAreaNavigationHeader.propTypes = {
+    title: PropTypes.string,
+    onBack: PropTypes.func,
+    icons: PropTypes.array,
+    showLeftMenu: PropTypes.bool,
+    leftMenuMode: PropTypes.oneOf(['back', 'menu']),
+    showRightMenu: PropTypes.bool,
+    membersNavigation: PropTypes.object,
+    drawerNavigation: PropTypes.object
+};
+
+MembersAreaNavigationHeader.defaultProps = {
+    title: '',
+    onBack: null,
+    icons: [],
+    showLeftMenu: false,
+    leftMenuMode: 'menu',
+    showRightMenu: false,
+    membersNavigation: {},
+    drawerNavigation: {}
+};
+
+export default MembersAreaNavigationHeader;
