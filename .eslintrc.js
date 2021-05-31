e58bcaddc77b18313f50cff5c0bd12658a702606
James <james.mahy@cevo.co.uk>
2021-05-31 15:13:04 +0100

    Linting fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-    root: true,
+	root: true,
 	extends: ['@react-native-community', 'prettier'],
 	plugins: ['prettier'],
 	env: { jest: true },
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	parser: 'babel-eslint',
 };
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,10 +4,14 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+	require('../App/components/ProfilePicture/ProfilePicture.stories');
 	require('../App/components/SocialButton/SocialButton.stories');
 }
 
-const stories = ['../App/components/SocialButton/SocialButton.stories'];
+const stories = [
+	'../App/components/ProfilePicture/ProfilePicture.stories',
+	'../App/components/SocialButton/SocialButton.stories',
+];
 
 module.exports = {
 	loadStories,
